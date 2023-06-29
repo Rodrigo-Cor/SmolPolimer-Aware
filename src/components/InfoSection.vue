bd587633e411f3f3273e698b4294e18cfa5e0494
dqomopb <brokepeny@gmail.com>
2023-06-29 00:05:18 -0600

    Agregué componente InfoSection para la sección de divulgación, modifiqué el AboutView.vue

---
+++ src/components/InfoSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col align-self-center">
+        <img
+          :src= img 
+          class="img-fluid"
+          alt="..."
+        />
+      </div>
+      <div class="col align-self-center">
+        <h4>
+            {{txt}}
+        </h4>
+      </div>
+    </div>
+    <div class="row"></div>
+  </div>
+</template>
+<script>
+    export default{
+        name:'InfoSection',
+        props:{
+            txt: 'String',
+            img: 'String'
+        }
+    }
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="about">
-    <h1></h1>
-  </div>
+  <InfoSection
+    txt="Desde 2016, se ha registrado un alarmante aumento en el uso de plásticos, especialmente productos de un solo uso, que han provocado la acumulación masiva en el medio ambiente."
+    img= "../assets/WaterPlasticPollution.jpg"
+  ></InfoSection>
 </template>
-<style scoped>
-</style>
+<script>
+import InfoSection from "@/components/InfoSection.vue";
+export default {
+  name: "AboutView",
+  components: {
+    InfoSection,
+  },
+};
+</script>
